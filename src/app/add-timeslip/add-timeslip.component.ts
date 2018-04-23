6f4e6389eb318d90f0d80139ff716d309483f8d1
Clayton Winterbotham <clayton@Claytons-MacBook-Air.local>
2018-04-22 19:32:31 -0700

    ng select integration
    ng select integration

--- src/app/add-timeslip/add-timeslip.component.ts
+++ src/app/add-timeslip/add-timeslip.component.ts
@@ -9,10 +9,31 @@ import { NgModel } from '@angular/forms';
 export class AddTimeslipComponent implements OnInit {
 
   constructor() { }
+  tags = [
+    { value: 1, label: 'tags1' },
+    { value: 2, label: 'tags2' },
+    { value: 3, label: 'tags3' },
+    { value: 4, label: 'tags4' },
+    { value: 5, label: 'tags5' },
+    { value: 6, label: 'tags6' }
+  ];
+
   projects = [
-    { value: 1, label: 'project1' },
-    { value: 2, label: 'project2' },
-    { value: 3, label: 'project3' }
+    { value: 1, label: 'projects1' },
+    { value: 2, label: 'projects2' },
+    { value: 3, label: 'projects3' },
+    { value: 4, label: 'projects4' },
+    { value: 5, label: 'projects5' },
+    { value: 6, label: 'projects6' }
+  ];
+
+  wbis = [
+    { value: 1, label: 'wbis1' },
+    { value: 2, label: 'wbis2' },
+    { value: 3, label: 'wbis3' },
+    { value: 4, label: 'wbis4' },
+    { value: 5, label: 'wbis5' },
+    { value: 6, label: 'wbis6' }
   ];
 
   ngOnInit() {
