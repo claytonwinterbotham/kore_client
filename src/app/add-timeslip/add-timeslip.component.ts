ddc20fb5fbccaf54b7918e4ed4561e3c99c45f32
Clayton Winterbotham <clayton@claytons-air.wifi.bcit.ca>
2018-04-24 09:53:53 -0700

    angular5 calender integration
    angular5 calender integration

--- src/app/add-timeslip/add-timeslip.component.ts
+++ src/app/add-timeslip/add-timeslip.component.ts
@@ -1,30 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit, 
+  ChangeDetectionStrategy,
+  ViewChild,
+  TemplateRef
+} from '@angular/core';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
 import { NgModel } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarEventTitleFormatter
+} from 'angular-calendar';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
-  selector: 'app-add-timeslip',
-  templateUrl: './add-timeslip.component.html',
-  styleUrls: ['./add-timeslip.component.css']
+  selector: 'calendar-component',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['add-timeslip.component.css'],
+  templateUrl: 'add-timeslip.component.html'
 })
-export class AddTimeslipComponent implements OnInit {
 
-  constructor() { }
-  tags = [
-    { value: 1, label: 'tags1' },
-    { value: 2, label: 'tags2' },
-    { value: 3, label: 'tags3' },
-    { value: 4, label: 'tags4' },
-    { value: 5, label: 'tags5' },
-    { value: 6, label: 'tags6' }
-  ];
+export class AddTimeslipComponent{
+  @ViewChild('modalContent') modalContent: TemplateRef<any>;
 
   projects = [
-    { value: 1, label: 'projects1' },
-    { value: 2, label: 'projects2' },
-    { value: 3, label: 'projects3' },
-    { value: 4, label: 'projects4' },
-    { value: 5, label: 'projects5' },
-    { value: 6, label: 'projects6' }
+    { value: 1, label: 'wbis1' },
+    { value: 2, label: 'wbis2' },
+    { value: 3, label: 'wbis3' },
+    { value: 4, label: 'wbis4' },
+    { value: 5, label: 'wbis5' },
+    { value: 6, label: 'wbis6' }
   ];
 
   wbis = [
@@ -36,6 +68,99 @@ export class AddTimeslipComponent implements OnInit {
     { value: 6, label: 'wbis6' }
   ];
 
-  ngOnInit() {
+  view: string = 'month';
+
+  viewDate: Date = new Date();
+
+  modalData: {
+    action: string;
+    event: CalendarEvent;
+  };
+
+  actions: CalendarEventAction[] = [
+    {
+      label: '<i class="fas fa-pencil-alt"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.handleEvent('Edited', event);
+      }
+    },
+    {
+      label: '<i class="fa fa-fw fa-times"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.events = this.events.filter(iEvent => iEvent !== event);
+        this.handleEvent('Deleted', event);
+      }
+    }
+  ];
+
+  refresh: Subject<any> = new Subject();
+
+  events: CalendarEvent[] = [
+    // {
+    //   start: subDays(startOfDay(new Date()), 1),
+    //   end: addDays(new Date(), 1),
+    //   title: 'title',
+    //   project: 'Atlanta',
+    //   wbi: 'Projects',
+    //   description: 'Worked on projects',
+    //   color: colors.red,
+    //   actions: this.actions
+    // }
+  ];
+
+  activeDayIsOpen: boolean = true;
+
+  constructor(private modal: NgbModal) {}
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
+  }
+
+  eventTimesChanged({
+    event,
+    newStart,
+    newEnd
+  }: CalendarEventTimesChangedEvent): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.handleEvent('Dropped or resized', event);
+    this.refresh.next();
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
+  }
+
+  addEvent(): void {
+    this.events.push({
+      project: {
+        value: ''
+      },
+      wbi: {
+        value: ''
+      },
+      description: '',
+      start: startOfDay(new Date()),
+      end: endOfDay(new Date()),
+      color: colors.red,
+      draggable: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      actions: this.actions
+    });
+    this.refresh.next();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'angular-calendar';
+import { CommonModule } from '@angular/common';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarUtilsModule } from './calendar-utils/module';
+
 
 import { AppComponent } from './app.component';
 import { AddTimeslipComponent } from './add-timeslip/add-timeslip.component';
 import { ViewTimeslipComponent } from './view-timeslip/view-timeslip.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routing } from './app.routing';
 import { ViewProjectsComponent } from './view-projects/view-projects.component';
 import { ViewWmbsComponent } from './view-wmbs/view-wmbs.component';
@@ -26,14 +32,19 @@ import { NgbdTimepickerMeridian } from './widgets/timepicker/timepicker';
     ViewProjectsComponent,
     ViewWmbsComponent,
     NgbdDatepickerPopup,
-    NgbdTimepickerMeridian
+    NgbdTimepickerMeridian,
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     routing,
     FormsModule,
-    NgSelectModule
+    NgSelectModule,
+    CalendarModule.forRoot(),
+    CommonModule,
+    NgbModalModule.forRoot(),
+    CalendarUtilsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
