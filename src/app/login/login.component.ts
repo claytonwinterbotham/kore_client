ed5d34620649f222e468080ecc37087c19a8dc4d
kevin-kline <21stcenturykline@gmail.com>
2018-04-27 14:26:46 -0700

    worked on login and register components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+//import { MyUserService } from '../services/app.userservice';
+import { MyUserService } from '../services/app.userservice'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [MyUserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+   email: string;
+   password: string;
+  _userDataService: MyUserService;
 
-  ngOnInit() {
+    constructor(userDataService: MyUserService) { 
+    this._userDataService = userDataService;
   }
 
+  ngOnInit() {
+     this.email = "";
+     this.password = "";
+    
+  }
+
+  login(){
+    //add logic
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { MyUserService, UserModel } from '../services/app.userservice';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [MyUserService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  newUser: UserModel;
 
-  ngOnInit() {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  firstName: string;
+  lastName: string;
+
+  _userDataService: MyUserService;
+
+  constructor(userDataService: MyUserService, private router: Router) { 
+      this._userDataService = userDataService;
   }
 
+  ngOnInit() {
+    this.email = "";
+    this.password = "";
+    this.confirmPassword = "";
+    this.firstName = "";
+    this.lastName = "";
+  }
+
+  register() {
+      let newUser = {
+        "UserId": null,
+        "Email": this.email,
+        "Password": this.password,
+        "FirstName": this.firstName,
+        "LastName": this.lastName
+      }
+      this._userDataService.registerUser(newUser).subscribe(
+        data => {
+          console.log(data)
+          this.router.navigate(['home'])
+        },
+        error => {
+          alert(error)
+        },
+        //3. execute final instructions
+        () => {
+          console.log("Finished")          
+        }
+
+      )
+  }
 }
