e7858b07dc12cd4cfebc9aa9113117058d3a6d72
karl <karlxu0410@gmail.com>
2018-05-08 15:38:40 -0700

    fix for new user.

--- src/app/add-timeslip/add-timeslip.component.ts
+++ src/app/add-timeslip/add-timeslip.component.ts
@@ -415,6 +415,9 @@ export class AddTimeslipComponent{
   }
 
   validateNewEvent (validationEvent : CalendarEvent, events : Array<CalendarEvent<{ timeSlipId: string,WBIId : string }>>): boolean{
+    if (events.length == 0){
+      return true;
+    }
     for (let oneEvent of events){
       if (oneEvent.start>= validationEvent.end || oneEvent.end <= validationEvent.start){
 
@@ -557,7 +560,11 @@ export class AddTimeslipComponent{
     this.timeSlipService.getTimeSlipsByUserId(this.userId).subscribe(
       data=> {
         console.log(data);
-        this.allTimeSlips = data;
+        if (data[0]["newTimesheetEntryId"] == null){
+          this.allTimeSlips = "";
+        }else {
+          this.allTimeSlips = data;
+        }
         this.showInCalendar();
       },
     error => {
@@ -579,13 +586,16 @@ export class AddTimeslipComponent{
   }
 
   showInCalendar(){
-    if (this.allTimeSlips.length == 0){
+    if (this.allTimeSlips == ""){
       return ;
+    }else {
+      console.log(this.allTimeSlips);
+      for (let oneTimeSlip of this.allTimeSlips){
+        //this.getTitleName(oneTimeSlip.newRemarks,oneTimeSlip.newStartTask,oneTimeSlip.newEndTask,oneTimeSlip.newTimesheetEntryId,oneTimeSlip.newChangeRequestId,colors.blue,true);
+        this.addNewEvent(oneTimeSlip.newRemarks,oneTimeSlip.newStartTask,oneTimeSlip.newEndTask,oneTimeSlip.newTimesheetEntryId,oneTimeSlip.newChangeRequestId,oneTimeSlip.wbiName, colors.blue);
+      }
     }
-    for (let oneTimeSlip of this.allTimeSlips){
-      //this.getTitleName(oneTimeSlip.newRemarks,oneTimeSlip.newStartTask,oneTimeSlip.newEndTask,oneTimeSlip.newTimesheetEntryId,oneTimeSlip.newChangeRequestId,colors.blue,true);
-      this.addNewEvent(oneTimeSlip.newRemarks,oneTimeSlip.newStartTask,oneTimeSlip.newEndTask,oneTimeSlip.newTimesheetEntryId,oneTimeSlip.newChangeRequestId,oneTimeSlip.wbiName, colors.blue);
-    }
+
     console.log(this.events);
   }
 
